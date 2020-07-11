53af81c38b33a45edca97641794f00447c9332dd
YanuarWanda <yanuar.wanda2@gmail.com>
2020-07-11 16:01:11 +0700

    Sampai UTS

---
+++ ProvisP1/src/provisp1/kelasutama.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package provisp1;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author yanua
+ */
+public class kelasutama {
+    public static void main(String[] args) {
+        // System.out.println("Hallo Dunia");
+        
+        // Menghidupkan objek
+        // frameutama futama = new frameutama(); // Instansiasi objek
+        // futama.tampil();
+        
+        // frameutama2 futama2 = new frameutama2();
+        // futama2.setVisible(true);
+
+        JOptionPane.showMessageDialog(null, "Selamat Belajar Provis");
+    }
+}
